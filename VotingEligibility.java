aac3fe27a815c7390c813dea17725ed7f2515cc4
8095128299 <inagalagianand030@gamil.com>
2025-06-10 22:07:38 +0530

    svd

---
+++ VotingEligibility.java
@@ -0,0 +1,8 @@
+public class VotingEligibility {
+ public static void main(String[] args) {
+ int age = 20;
+ if (age >= 18) {
+ System.out.println("Eligible to vote.");
+ } else {
+ System.out.println("Not eligible to vote.");
+}}}
